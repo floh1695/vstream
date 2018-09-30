32253ad249101b5ef3caef74aa2807b9d8006296
Charles Bayley <cbayley95@gmail.com>
2018-09-29 20:48:02 -0400

    Moved in a direction I now think is wrong, commiting so this is recoverable

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,59 @@
+enum Mode {
+  Normal,
+  Insert,
+}
+
+class Editor {
+  public mode: Mode = Mode.Normal;
+
+  public buffer: string;
+  public cursor: number = 0;
+
+  public constructor(buffer: string) {
+    this.buffer = buffer;
+  }
+
+  public interpret(text: string): Editor {
+    const mode = this.mode;
+
+    if (mode === Mode.Normal) {
+      this.interpretNormal(text);
+    } else if (mode === Mode.Insert) {
+      this.interpretInsert(text);
+    }
+    
+    return this;
+  }
+
+  private interpretNormal(text: string) {
+    text
+      .split('')
+      .forEach((char, index) => {
+        if (char === 'i') {
+          this.mode = Mode.Insert;
+
+          const leftover = text
+            .split('')
+            .slice(index + 1)
+            .join('');
+
+          this.interpretInsert(leftover);
+        }
+      });
+  }
+
+  private interpretInsert(text: string) {
+    console.log(text);
+  }
+}
+
 const main = (): void => {
   console.log('Hello world!');
+  const editor = new Editor('');
+
+  editor
+    .interpret('iabc')
+    .interpret('abci123');
 }
 
 main();
